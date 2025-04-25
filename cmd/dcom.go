865b523bdd4b006661ef8fbf76d24e4b7ad1cf8e
Bryan McNulty <bryanmcnulty@protonmail.com>
2025-04-25 10:44:52 -0500

    Small cosmetic changes in Cobra cmds

--- cmd/dcom.go
+++ cmd/dcom.go
@@ -1,79 +1,80 @@
 package cmd
 
 import (
-  "context"
-  "github.com/FalconOpsLLC/goexec/pkg/goexec"
-  dcomexec "github.com/FalconOpsLLC/goexec/pkg/goexec/dcom"
-  "github.com/oiweiwei/go-msrpc/ssp/gssapi"
-  "github.com/spf13/cobra"
+	"context"
+	"github.com/FalconOpsLLC/goexec/pkg/goexec"
+	dcomexec "github.com/FalconOpsLLC/goexec/pkg/goexec/dcom"
+	"github.com/oiweiwei/go-msrpc/ssp/gssapi"
+	"github.com/spf13/cobra"
 )
 
 func dcomCmdInit() {
-  cmdFlags[dcomCmd] = []*flagSet{
-    defaultAuthFlags,
-    defaultLogFlags,
-    defaultNetRpcFlags,
-  }
-  dcomMmcCmdInit()
+	cmdFlags[dcomCmd] = []*flagSet{
+		defaultAuthFlags,
+		defaultLogFlags,
+		defaultNetRpcFlags,
+	}
+	dcomMmcCmdInit()
 
-  dcomCmd.PersistentFlags().AddFlagSet(defaultAuthFlags.Flags)
-  dcomCmd.PersistentFlags().AddFlagSet(defaultLogFlags.Flags)
-  dcomCmd.PersistentFlags().AddFlagSet(defaultNetRpcFlags.Flags)
-  dcomCmd.AddCommand(dcomMmcCmd)
+	dcomCmd.PersistentFlags().AddFlagSet(defaultAuthFlags.Flags)
+	dcomCmd.PersistentFlags().AddFlagSet(defaultLogFlags.Flags)
+	dcomCmd.PersistentFlags().AddFlagSet(defaultNetRpcFlags.Flags)
+	dcomCmd.AddCommand(dcomMmcCmd)
 }
 
 func dcomMmcCmdInit() {
-  dcomMmcExecFlags := newFlagSet("Execution")
+	dcomMmcExecFlags := newFlagSet("Execution")
 
-  registerExecutionFlags(dcomMmcExecFlags.Flags)
-  registerExecutionOutputFlags(dcomMmcExecFlags.Flags)
+	registerExecutionFlags(dcomMmcExecFlags.Flags)
+	registerExecutionOutputFlags(dcomMmcExecFlags.Flags)
 
-  dcomMmcExecFlags.Flags.StringVar(&dcomMmc.WorkingDirectory, "directory", `C:\`, "Working `directory`")
-  dcomMmcExecFlags.Flags.StringVar(&dcomMmc.WindowState, "window", "Minimized", "Window state")
+	dcomMmcExecFlags.Flags.StringVar(&dcomMmc.WorkingDirectory, "directory", `C:\`, "Working `directory`")
+	dcomMmcExecFlags.Flags.StringVar(&dcomMmc.WindowState, "window", "Minimized", "Window state")
 
-  cmdFlags[dcomMmcCmd] = []*flagSet{
-    dcomMmcExecFlags,
-    defaultAuthFlags,
-    defaultLogFlags,
-    defaultNetRpcFlags,
-  }
+	cmdFlags[dcomMmcCmd] = []*flagSet{
+		dcomMmcExecFlags,
+		defaultAuthFlags,
+		defaultLogFlags,
+		defaultNetRpcFlags,
+	}
 
-  dcomMmcCmd.Flags().AddFlagSet(dcomMmcExecFlags.Flags)
+	dcomMmcCmd.Flags().AddFlagSet(dcomMmcExecFlags.Flags)
 }
 
 var (
-  dcomMmc dcomexec.DcomMmc
+	dcomMmc dcomexec.DcomMmc
 
-  dcomCmd = &cobra.Command{
-    Use:     "dcom",
-    Short:   "Execute with Distributed Component Object Model (MS-DCOM)",
-    GroupID: "module",
-    Args:    cobra.NoArgs,
-  }
+	dcomCmd = &cobra.Command{
+		Use:   "dcom",
+		Short: "Execute with Distributed Component Object Model (MS-DCOM)",
+		Long: `Description:
+  The dcom module uses exposed Distributed Component Object Model (DCOM) objects to spawn processes.`,
+		GroupID: "module",
+		Args:    cobra.NoArgs,
+	}
 
-  dcomMmcCmd = &cobra.Command{
-    Use:   "mmc [target]",
-    Short: "Execute with the DCOM MMC20.Application object",
-    Long: `Description:
+	dcomMmcCmd = &cobra.Command{
+		Use:   "mmc [target]",
+		Short: "Execute with the DCOM MMC20.Application object",
+		Long: `Description:
   The mmc method uses the exposed MMC20.Application object to call Document.ActiveView.ShellExec,
-  and ultimately spawn a process on the remote host.
-`,
-    Args: args(
-      argsRpcClient("host"),
-      argsOutput("smb"),
-    ),
-    Run: func(cmd *cobra.Command, args []string) {
-      dcomMmc.Dcom.Client = &rpcClient
-      dcomMmc.IO = exec
+  and ultimately spawn a process on the remote host.`,
+		Args: args(
+			argsRpcClient("host"),
+			argsOutput("smb"),
+		),
+		Run: func(cmd *cobra.Command, args []string) {
+			dcomMmc.Dcom.Client = &rpcClient
+			dcomMmc.IO = exec
 
-      ctx := log.With().
-        Str("module", "dcom").
-        Str("method", "mmc").
-        Logger().WithContext(gssapi.NewSecurityContext(context.Background()))
+			ctx := log.With().
+				Str("module", "dcom").
+				Str("method", "mmc").
+				Logger().WithContext(gssapi.NewSecurityContext(context.Background()))
 
-      if err := goexec.ExecuteCleanMethod(ctx, &dcomMmc, &exec); err != nil {
-        log.Fatal().Err(err).Msg("Operation failed")
-      }
-    },
-  }
+			if err := goexec.ExecuteCleanMethod(ctx, &dcomMmc, &exec); err != nil {
+				log.Fatal().Err(err).Msg("Operation failed")
+			}
+		},
+	}
 )
--- cmd/root.go
+++ cmd/root.go
@@ -1,25 +1,25 @@
 package cmd
 
 import (
-  "fmt"
-  "github.com/FalconOpsLLC/goexec/pkg/goexec"
-  "github.com/FalconOpsLLC/goexec/pkg/goexec/dce"
-  "github.com/FalconOpsLLC/goexec/pkg/goexec/smb"
-  "github.com/RedTeamPentesting/adauth"
-  "github.com/google/uuid"
-  "github.com/oiweiwei/go-msrpc/ssp"
-  "github.com/oiweiwei/go-msrpc/ssp/gssapi"
-  "github.com/rs/zerolog"
-  "github.com/spf13/cobra"
-  "github.com/spf13/pflag"
-  "golang.org/x/term"
-  "io"
-  "os"
+	"fmt"
+	"github.com/FalconOpsLLC/goexec/pkg/goexec"
+	"github.com/FalconOpsLLC/goexec/pkg/goexec/dce"
+	"github.com/FalconOpsLLC/goexec/pkg/goexec/smb"
+	"github.com/RedTeamPentesting/adauth"
+	"github.com/google/uuid"
+	"github.com/oiweiwei/go-msrpc/ssp"
+	"github.com/oiweiwei/go-msrpc/ssp/gssapi"
+	"github.com/rs/zerolog"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"golang.org/x/term"
+	"io"
+	"os"
 )
 
 type flagSet struct {
-  Label string
-  Flags *pflag.FlagSet
+	Label string
+	Flags *pflag.FlagSet
 }
 
 const helpTemplate = `Usage:{{if .Runnable}}
@@ -51,47 +51,47 @@ Use "{{.CommandPath}} [command] --help" for more information about a command.{{e
 `
 
 var (
-  cmdFlags = make(map[*cobra.Command][]*flagSet)
+	cmdFlags = make(map[*cobra.Command][]*flagSet)
 
-  defaultAuthFlags, defaultLogFlags, defaultNetRpcFlags *flagSet
+	defaultAuthFlags, defaultLogFlags, defaultNetRpcFlags *flagSet
 
-  returnCode int
+	returnCode int
 
-  // === IO ===
-  stageFilePath string
-  outputMethod  string
-  outputPath    string
-  // ==========
+	// === IO ===
+	stageFilePath string
+	outputMethod  string
+	outputPath    string
+	// ==========
 
-  // === Logging ===
-  logJson   bool           // Log output in JSON lines
-  logDebug  bool           // Output debug log messages
-  logQuiet  bool           // Suppress logging output
-  logOutput string         // Log output file
-  logLevel  zerolog.Level  = zerolog.InfoLevel
-  logFile   io.WriteCloser = os.Stderr
-  log       zerolog.Logger
-  // ===============
+	// === Logging ===
+	logJson   bool           // Log output in JSON lines
+	logDebug  bool           // Output debug log messages
+	logQuiet  bool           // Suppress logging output
+	logOutput string         // Log output file
+	logLevel  zerolog.Level  = zerolog.InfoLevel
+	logFile   io.WriteCloser = os.Stderr
+	log       zerolog.Logger
+	// ===============
 
-  // === Network ===
-  proxy     string
-  rpcClient dce.Client
-  smbClient smb.Client
-  // ===============
+	// === Network ===
+	proxy     string
+	rpcClient dce.Client
+	smbClient smb.Client
+	// ===============
 
-  exec = goexec.ExecutionIO{
-    Input:  new(goexec.ExecutionInput),
-    Output: new(goexec.ExecutionOutput),
-  }
+	exec = goexec.ExecutionIO{
+		Input:  new(goexec.ExecutionInput),
+		Output: new(goexec.ExecutionOutput),
+	}
 
-  adAuthOpts *adauth.Options
-  credential *adauth.Credential
-  target     *adauth.Target
+	adAuthOpts *adauth.Options
+	credential *adauth.Credential
+	target     *adauth.Target
 
-  rootCmd = &cobra.Command{
-    Use:   "goexec",
-    Short: `goexec - Windows remote execution multitool`,
-    Long: `
+	rootCmd = &cobra.Command{
+		Use:   "goexec",
+		Short: `goexec - Windows remote execution multitool`,
+		Long: `
   ___ ___ ___ _ _ ___ ___
  | . | . | -_|_'_| -_|  _|
  |_  |___|___|_,_|___|___|
@@ -104,144 +104,144 @@ Authors: FalconOps LLC (@FalconOpsLLC),
   while providing an extremely flexible CLI and a strong focus on OPSEC.
 `,
 
-    PersistentPreRunE: func(cmd *cobra.Command, args []string) (err error) {
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) (err error) {
 
-      // Parse logging options
-      {
-        if logOutput != "" {
-          logFile, err = os.OpenFile(logOutput, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
-          if err != nil {
-            return
-          }
-          logJson = true
-        }
-        if logQuiet {
-          logLevel = zerolog.ErrorLevel
-        } else if logDebug {
-          logLevel = zerolog.DebugLevel
-        }
-        if logJson {
-          log = zerolog.New(logFile).With().Timestamp().Logger()
-        } else {
-          log = zerolog.New(zerolog.ConsoleWriter{Out: logFile}).With().Timestamp().Logger()
-        }
-        log = log.Level(logLevel)
-      }
+			// Parse logging options
+			{
+				if logOutput != "" {
+					logFile, err = os.OpenFile(logOutput, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+					if err != nil {
+						return
+					}
+					logJson = true
+				}
+				if logQuiet {
+					logLevel = zerolog.ErrorLevel
+				} else if logDebug {
+					logLevel = zerolog.DebugLevel
+				}
+				if logJson {
+					log = zerolog.New(logFile).With().Timestamp().Logger()
+				} else {
+					log = zerolog.New(zerolog.ConsoleWriter{Out: logFile}).With().Timestamp().Logger()
+				}
+				log = log.Level(logLevel)
+			}
 
-      if proxy != "" {
-        rpcClient.Proxy = proxy
-        smbClient.Proxy = proxy
-      }
+			if proxy != "" {
+				rpcClient.Proxy = proxy
+				smbClient.Proxy = proxy
+			}
 
-      if outputPath != "" {
-        if outputMethod == "smb" {
-          if exec.Output.RemotePath == "" {
-            exec.Output.RemotePath = `C:\Windows\Temp\` + uuid.NewString()
-          }
-          exec.Output.Provider = &smb.OutputFileFetcher{
-            Client:           &smbClient,
-            Share:            `ADMIN$`, // TODO: dynamic
-            SharePath:        `C:\Windows`,
-            File:             exec.Output.RemotePath,
-            DeleteOutputFile: !exec.Output.NoDelete,
-          }
-        }
-      }
-      return
-    },
+			if outputPath != "" {
+				if outputMethod == "smb" {
+					if exec.Output.RemotePath == "" {
+						exec.Output.RemotePath = `C:\Windows\Temp\` + uuid.NewString()
+					}
+					exec.Output.Provider = &smb.OutputFileFetcher{
+						Client:           &smbClient,
+						Share:            `ADMIN$`, // TODO: dynamic
+						SharePath:        `C:\Windows`,
+						File:             exec.Output.RemotePath,
+						DeleteOutputFile: !exec.Output.NoDelete,
+					}
+				}
+			}
+			return
+		},
 
-    PersistentPostRun: func(cmd *cobra.Command, args []string) {
-      if err := logFile.Close(); err != nil {
-        // ...
-      }
-      if err := exec.Input.StageFile.Close(); err != nil {
-        // ...
-      }
-    },
-  }
+		PersistentPostRun: func(cmd *cobra.Command, args []string) {
+			if err := logFile.Close(); err != nil {
+				// ...
+			}
+			if err := exec.Input.StageFile.Close(); err != nil {
+				// ...
+			}
+		},
+	}
 )
 
 func newFlagSet(name string) *flagSet {
-  flags := pflag.NewFlagSet(name, pflag.ExitOnError)
-  flags.SortFlags = false
-  return &flagSet{
-    Label: name,
-    Flags: flags,
-  }
+	flags := pflag.NewFlagSet(name, pflag.ExitOnError)
+	flags.SortFlags = false
+	return &flagSet{
+		Label: name,
+		Flags: flags,
+	}
 }
 
 func init() {
-  // Auth init
-  {
-    gssapi.AddMechanism(ssp.SPNEGO)
-    gssapi.AddMechanism(ssp.NTLM)
-    gssapi.AddMechanism(ssp.KRB5)
-  }
+	// Auth init
+	{
+		gssapi.AddMechanism(ssp.SPNEGO)
+		gssapi.AddMechanism(ssp.NTLM)
+		gssapi.AddMechanism(ssp.KRB5)
+	}
 
-  // Cobra init
-  {
-    cobra.EnableCommandSorting = false
-    {
-      defaultNetRpcFlags = newFlagSet("Network")
-      registerNetworkFlags(defaultNetRpcFlags.Flags)
-    }
-    {
-      defaultLogFlags = newFlagSet("Logging")
-      registerLoggingFlags(defaultLogFlags.Flags)
-    }
-    {
-      defaultAuthFlags = newFlagSet("Authentication")
-      adAuthOpts = &adauth.Options{
-        Debug: log.Debug().Msgf,
-      }
-      adAuthOpts.RegisterFlags(defaultAuthFlags.Flags)
-    }
+	// Cobra init
+	{
+		cobra.EnableCommandSorting = false
+		{
+			defaultNetRpcFlags = newFlagSet("Network")
+			registerNetworkFlags(defaultNetRpcFlags.Flags)
+		}
+		{
+			defaultLogFlags = newFlagSet("Logging")
+			registerLoggingFlags(defaultLogFlags.Flags)
+		}
+		{
+			defaultAuthFlags = newFlagSet("Authentication")
+			adAuthOpts = &adauth.Options{
+				Debug: log.Debug().Msgf,
+			}
+			adAuthOpts.RegisterFlags(defaultAuthFlags.Flags)
+		}
 
-    modules := &cobra.Group{
-      ID:    "module",
-      Title: "Execution Commands:",
-    }
-    rootCmd.AddGroup(modules)
+		modules := &cobra.Group{
+			ID:    "module",
+			Title: "Execution Commands:",
+		}
+		rootCmd.AddGroup(modules)
 
-    cmdFlags[rootCmd] = []*flagSet{
-      defaultLogFlags,
-      defaultAuthFlags,
-    }
+		cmdFlags[rootCmd] = []*flagSet{
+			defaultLogFlags,
+			defaultAuthFlags,
+		}
 
-    cobra.AddTemplateFunc("flags", func(fs *pflag.FlagSet) string {
-      if width, _, err := term.GetSize(int(os.Stdout.Fd())); err == nil {
-        return fs.FlagUsagesWrapped(width - 1)
-      }
-      return fs.FlagUsagesWrapped(80 - 1)
-    })
+		cobra.AddTemplateFunc("flags", func(fs *pflag.FlagSet) string {
+			if width, _, err := term.GetSize(int(os.Stdout.Fd())); err == nil {
+				return fs.FlagUsagesWrapped(width - 1)
+			}
+			return fs.FlagUsagesWrapped(80 - 1)
+		})
 
-    cobra.AddTemplateFunc("cmdFlags", func(cmd *cobra.Command) []*flagSet {
-      return cmdFlags[cmd]
-    })
+		cobra.AddTemplateFunc("cmdFlags", func(cmd *cobra.Command) []*flagSet {
+			return cmdFlags[cmd]
+		})
 
-    rootCmd.InitDefaultVersionFlag()
-    rootCmd.InitDefaultHelpCmd()
-    rootCmd.SetHelpTemplate("{{if (ne .Long \"\")}}{{.Long}}\n{{end}}" + helpTemplate)
-    rootCmd.SetUsageTemplate(helpTemplate)
+		rootCmd.InitDefaultVersionFlag()
+		rootCmd.InitDefaultHelpCmd()
+		rootCmd.SetHelpTemplate("{{if (ne .Long \"\")}}{{.Long}}\n\n{{end}}" + helpTemplate)
+		rootCmd.SetUsageTemplate(helpTemplate)
 
-    // Modules init
-    {
-      dcomCmdInit()
-      rootCmd.AddCommand(dcomCmd)
-      wmiCmdInit()
-      rootCmd.AddCommand(wmiCmd)
-      scmrCmdInit()
-      rootCmd.AddCommand(scmrCmd)
-      tschCmdInit()
-      rootCmd.AddCommand(tschCmd)
-    }
-  }
+		// Modules init
+		{
+			dcomCmdInit()
+			rootCmd.AddCommand(dcomCmd)
+			wmiCmdInit()
+			rootCmd.AddCommand(wmiCmd)
+			scmrCmdInit()
+			rootCmd.AddCommand(scmrCmd)
+			tschCmdInit()
+			rootCmd.AddCommand(tschCmd)
+		}
+	}
 }
 
 func Execute() {
-  if err := rootCmd.Execute(); err != nil {
-    fmt.Println(err)
-    os.Exit(1)
-  }
-  os.Exit(returnCode)
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	os.Exit(returnCode)
 }
